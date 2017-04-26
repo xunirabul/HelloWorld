d65d79d1c4e92be4339f155961a4873caeb381b9
rinux <rinux@mail.ru>
2017-04-26 11:14:24 +0400

    init

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HelloWorld
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtText.Text = txtInput.Text;
+        }
+    }
+}
