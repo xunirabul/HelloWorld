a683948d77aa3b69349750d164dadd7643f60175
rinux <rinux@mail.ru>
2017-04-27 10:39:38 +0400

    github

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace HelloWorld
             txtText.Text = txtInput.Text; // comment add rrr
             // branch master
             // hello new branch
+            // remote
         }
     }
 }
